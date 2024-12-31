6437d29b8385bfdc0b45ce69937ece7d0c598785
katawutp <katawut.paramee@gmail.com.com>
2024-12-31 14:07:17 +0700

    3K v.1.1

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import './globals.css'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'TikTok Clone',
-  description: 'TikTok Clone',
+  title: '3K v.1.1',
+  description: 'ก๊อกๆๆ - KokKokKok ',
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
